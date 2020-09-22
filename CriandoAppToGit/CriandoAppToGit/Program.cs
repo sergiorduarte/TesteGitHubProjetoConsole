5b574300ecb7544cd7b068ebd4af2c05e7003bf0
Sérgio Rodrigues Duarte <seergiorodrigues@hotmail.com>
2020-09-22 15:30:30 -0300

    segundo commit

--- CriandoAppToGit/CriandoAppToGit/Program.cs
+++ CriandoAppToGit/CriandoAppToGit/Program.cs
@@ -10,7 +10,7 @@ namespace CriandoAppToGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("teste");
+            Console.WriteLine("Exemplo 2");
             Console.ReadLine();
         }
     }
